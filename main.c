e767c4dbc469287bc96d0b685dc9e934d8a45678
Leonardo Marotta <sen.aka@hotmail.com>
2019-09-04 12:10:28 -0300

    Atualização

--- main.c
+++ main.c
@@ -1,37 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct Lista{
+typedef struct ListaX{
     char nome[50];
     int fone;
     struct lista *prox;
 }lista;
 
-void insere(lista *topo, char nomes[50], char fone){
+void inserir(lista **topo, char nomes[50], char fones){
     
+    lista *listaS;
+    listaS = malloc(sizeof(lista));
+    strcpy(listaS->nome,nomes);
+    listaS->fone=fones;
+    if(*topo==NULL){
+        listaS->prox = NULL;
+        *topo = listaS;
+    }else{
+        listaS->prox = *topo;
+        *topo = listaS;
+    }
 }
+
+void listar(lista *topo){
+    lista *aux;
+    aux = topo;
+    if(aux==NULL){
+        
+    }else{
+        do{
+            printf("%s/%d->", aux->nome, aux->fone);
+        }while(aux!=NULL);
+    }
+}
+
+//void remover(){
+//   
+//}
+//
+//void buscar(){
+//    
+//}
 
 int main(int argc, char** argv) {
     lista *topo = NULL;
-    int opcao;
+    int opcao, fone;
+    char nome[50];
     do{
-        printf("---AGENDA---\n1-Inserir\n2-Listar\n3-Remover\n4-Buscar\n-5Sair\nOpcao:");
+        printf("---AGENDA---\n1-Inserir\n2-Listar\n3-Remover\n4-Buscar\n5-Sair\nOpcao:");
         scanf("%d", &opcao);
         switch(opcao){
             case 1:
-                insere();
+                printf("Digite nome que deseja inserir.\n");
+                scanf("%s", nome);
+                printf("\nDigite o telefone do contato que deseja inserir.\n");
+                scanf("%d", &fone);
+                inserir(&topo, nome[50], fone);
                 break;
             case 2:
-                listar();
+                listar(topo);
                 break;
-            case 3:
-                remove();
-                break;
-            case 4:
-                buscar();
-                break;
+            //case 3:
+            //    remover();
+            //    break;
+            //case 4:
+            //    buscar();
+            //    break;
         }
-    }while(opcao!=0)
+    }while(opcao!=0);
     
     return (EXIT_SUCCESS);
 }
